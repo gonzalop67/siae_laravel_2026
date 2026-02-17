54a3623c253f74d46f24dec662f74fd221deb380
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2026-02-17 09:08:08 -0500

    Migración de las tablas menus y menu_rol, creación del controlador MenuControlller y de los modelos Menu y MenuRol.

---
+++ app/Models/Admin/Menu.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    protected $fillable = [
+        'nombre',
+        'url',
+        'icono'
+    ];
+    protected $guarded = ['id'];
+}
---
+++ database/migrations/2026_02_16_154622_create_menus_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_id')->constrained('menus')->onDelete('cascade')->default(0);
+            $table->string('nombre', 50);
+            $table->string('url', 100);
+            $table->unsignedTinyInteger('orden')->default(0);
+            $table->string('icono', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
